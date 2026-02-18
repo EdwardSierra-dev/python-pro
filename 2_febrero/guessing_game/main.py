7d8cc885ddf81ec0ef3fd979d975c862c82ad963
Edward Sierra <dev.edwardsc@gmail.com>
2026-02-18 11:30:12 -0500

    Se mejora el reinicio de la aplicación cuando se gana o se pierde

--- 2_febrero/guessing_game/main.py
+++ 2_febrero/guessing_game/main.py
@@ -10,12 +10,9 @@ randomNumbers = []
 countCorrect = 0
 cutPrint = "-" * 30
 
-for i in range(3):
-  randomNumbers.append(generateRandomNum())
 
 print(cutPrint)
 print("Hi! I'd generated 3 random numbers! 😈")
-print(randomNumbers)
 print("If you guess 2 times you'll winner!!!")
 print(cutPrint)
 
@@ -25,11 +22,17 @@ if userOption.lower() == 'q':
   startGame = False
 else:
   while startGame:
+    countCorrect = 0
+    randomNumbers.clear()
+    for i in range(3):
+      randomNumbers.append(generateRandomNum())
+    print(randomNumbers)
     print("Game stared! 😎")
     for i in range(3):
       userNum = int(input(f"Guess the number {i + 1}: "))
       print(cutPrint)
       if userNum == randomNumbers[i]:
+        # Claro aquí solo compara en el input con la posición pero necesito buscarlo en todos los indices jeje cagada
         print(cutPrint)
         countCorrect += 1
       else:
@@ -39,14 +42,14 @@ else:
     if countCorrect >= 2:
       print("Congratulations! You win! 🥳")
       print(cutPrint)
-      userOption = input("Do you want to try? 😈\nPress 'Enter' to start or Type 'q' to exit: ")
+      userOption = input("Do you want to try again? 😈\nPress 'Enter' to start or Type 'q' to exit: ")
       print(cutPrint)
       if userOption.lower() == 'q':
         startGame = False
     else:
       print("Sorry! You lose! 😢")
       print(cutPrint)
-      userOption = input("Do you want to try? 😈\nPress 'Enter' to start or Type 'q' to exit: ")
+      userOption = input("Do you want to try again? 😈\nPress 'Enter' to start or Type 'q' to exit: ")
       print(cutPrint)
       if userOption.lower() == 'q':
         startGame = False
