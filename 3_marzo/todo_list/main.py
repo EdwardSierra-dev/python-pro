dfeebdf4338a20f1a80a98bc5b339c18fcf2e2e8
EdwardSc <dev.edwardsc@gmail.com>
2026-02-25 21:08:13 -0500

    Se continua con la todo list, se agrega validación de ID vacío y se termina con el primer requerimiento uncional y con todos los criterios de aceptacion

--- 3_marzo/todo_list/main.py
+++ 3_marzo/todo_list/main.py
@@ -1,4 +1,9 @@
 # Functions
+def is_valid_title(title):
+  if title == "":
+    return False
+  else:
+    return True
 
 # Variables
 tasksList = []
@@ -25,20 +30,24 @@ while True:
   if choice == "1":
     increment += 1
     title = input("Enter task title: ")
-    id = increment
-    status = False
-    task = Task(title, id, status)
-    tasksList.append(task)
-    print("Task added successfully!")
+    emptyTitle = is_valid_title(title)
+    if emptyTitle == True:
+      id = increment
+      status = False
+      task = Task(title, id, status)
+      tasksList.append(task)
+      print("Task added successfully!")
+    else:
+      print("El título no puede estár vacío")
 
   elif choice == "2":
     for task in tasksList:
       if task.status == True:
         checked = "X"
-        print(f"[{checked}] {task.title}")
+        print(f"[{checked}] {task.title} {task.id}")
         checked = ""
       else:
-        print(f"[{checked}] {task.title}")
+        print(f"[{checked}] {task.title} {task.id}")
 
   elif choice == "3":
     taskId = int(input("Join task ID: "))
