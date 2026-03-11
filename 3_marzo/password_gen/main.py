9e4b6a599c547000e4bdeaa9c96179d3f5b90418
Edward Sierra <dev.edwardsc@gmail.com>
2026-03-11 11:08:29 -0500

    Se termina con el projecto password gen, teniendo en cuenta los requerimientos y los bonus

--- 3_marzo/password_gen/main.py
+++ 3_marzo/password_gen/main.py
@@ -1,3 +1,7 @@
+# Imports
+import pyperclip
+from random import randrange
+
 # Dicts
 lowercase = "abcdefghijklmnopqrstuvwxyz"
 uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -5,29 +9,31 @@ numbers = "0123456789"
 symbols = "!@#$%^&*()"
 
 # Vars
-passwordLen = int(input("How long do you want the password to be? "))
-passwordParams = ""
-password = ""
+numberOfPasswords = int(input("How many passwords do you want to generate? "))
 
-includeUppercase = input("Include uppercase letters (y/n): ").lower()
-includeLowercase = input("Include lowercase letters (y/n): ").lower()
-includeNumbers = input("Include numbers (y/n): ").lower()
-includeSymbols = input("Include symbols (y/n): ").lower()
+for i in range(numberOfPasswords):
+  passwordLen = int(input("How long do you want the password to be? "))
+  passwordParams = ""
+  password = ""
 
-if includeUppercase == "n" and includeLowercase == "n" and includeNumbers == "n" and includeSymbols == "n":
-  print("Error: You must select at least one character type.")
-else:
-  if includeUppercase == "y" : passwordParams += lowercase
-  if includeLowercase == "y" : passwordParams += uppercase
-  if includeNumbers == "y" : passwordParams += numbers
-  if includeSymbols == "y" : passwordParams += symbols
+  # Set password params
+  includeUppercase = input("Include uppercase letters (y/n): ").lower()
+  includeLowercase = input("Include lowercase letters (y/n): ").lower()
+  includeNumbers = input("Include numbers (y/n): ").lower()
+  includeSymbols = input("Include symbols (y/n): ").lower()
 
-print(passwordParams)
+  # Validate the params selected and set the params accepted
+  if includeUppercase == "n" and includeLowercase == "n" and includeNumbers == "n" and includeSymbols == "n":
+    print("Error: You must select at least one character type.")
+  else:
+    if includeUppercase == "y" : passwordParams += lowercase
+    if includeLowercase == "y" : passwordParams += uppercase
+    if includeNumbers == "y" : passwordParams += numbers
+    if includeSymbols == "y" : passwordParams += symbols
 
+    for j in range(passwordLen):
+      password += passwordParams[randrange(0, len(passwordParams), 1)]
 
-#for key in CHARACTER_SETS:
-#  print(CHARACTER_SETS[key])
-
-# SE DEBE REALIZAR LA VALIDACION DE CADA DICT KEY->VALUE PARA CONCATENARLO EN UN SET UNICO Y VALIDO
-# QUE VAYA ACORDE A LA SECCIÓN DEL USUARIO.
-
+  print(f"Su contraseña {i + 1} es: {password}")
+  pyperclip.copy(password)
+  print("Password was copied in you clipboard!")
