e9dd2591c76e4cbed69c62490e3071a816c38908
Edward Sierra <dev.edwardsc@gmail.com>
2026-03-19 13:16:17 -0500

    Se avanza hasta el 80% en la creación del script para reiniciar las terminales

--- 3_marzo/script_telnet/gui_controller/main.py
+++ 3_marzo/script_telnet/gui_controller/main.py
@@ -1,28 +1,72 @@
-import paramiko
+import telnetlib
 import time
+import os
 
-client = paramiko.SSHClient()
+# Functions
+def ping(host):
+    response = os.system(f"ping -n 1 {host}")  # Windows
+    # response = os.system(f"ping -c 1 {host}")  # Linux/Mac
+    return response == 0
 
-# 1. Aceptar host automáticamente
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+HOST = "10.192.3.25"
+PORT = 23
+USER = "40884366"
+PASSWORD = "123456"
 
-# 2. Conectarse
-client.connect("10.192.3.25", username="40884366", password="123456")
+def main():
 
-# 3. Abrir terminal interactiva
-shell = client.invoke_shell()
+  if ping("10.192.3.25"):
+      print("✅ Controlador activo")
+      tn = telnetlib.Telnet(HOST, PORT)
 
-time.sleep(1)
+      time.sleep(1)
+      # print(tn.read_very_eager().decode('latin-1'))
 
-# 4. Enviar comando
-shell.send("whoami\n")
+      # Enviar usuario
+      tn.read_until("Operador".encode('latin-1'))
+      tn.write(b"40884366\r\n")
+      time.sleep(2)
+      # print(tn.read_very_eager().decode('latin-1'))
+      
+      # Enviar password
+      tn.write(b"123456\r\n")
+      time.sleep(5)
+      # print(tn.read_very_eager().decode('latin-1'))
 
-time.sleep(1)
+      # Enter letters menu
+      time.sleep(5)
+      # tn.read_until("PRINCIPAL".encode('latin-1'))
+      tn.write(b"\x10")
+      tn.write(b"\x10")
+      # print(tn.read_very_eager().decode('latin-1'))
 
-# 5. Leer respuesta
-if shell.recv_ready():
-    output = shell.recv(5000).decode()
-    print(output)
+      # Enter letter c
+      time.sleep(5)
+      # tn.read_until("TECLAS".encode('latin-1'))
+      tn.write(b"c\r\n")
+      # print(tn.read_very_eager().decode('latin-1'))
 
-# 6. Cerrar conexión
-client.close()
+      # Enter 1 (Funciones de terminal)
+      time.sleep(5)
+      # tn.read_until("Terminal".encode('latin-1'))
+      tn.write(b"1\r\n")
+      # print(tn.read_very_eager().decode('latin-1'))
+
+      # Enter 7 (Cargar Memoria del Terminal)
+      time.sleep(5)
+      tn.write(b"7\r\n")
+      # print(tn.read_very_eager().decode('latin-1'))
+
+      # Enter ID caja
+      time.sleep(5)
+      tn.write(b"103\r\n")
+      time.sleep(2)
+      # print(tn.read_very_eager().decode('latin-1'))
+
+      tn.close()
+
+  else:
+      print("❌ No responde")
+
+if __name__ == "__main__":
+    main()
