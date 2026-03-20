baf674b8ec7ea801022c9d464236be1ce9b18c11
Edward Sierra <dev.edwardsc@gmail.com>
2026-03-20 09:24:26 -0500

    Seguimos, ya descubrimos algo nuevo que nos puede ayudar a navegar entre los menús

--- 3_marzo/script_telnet/gui_controller/main.py
+++ 3_marzo/script_telnet/gui_controller/main.py
@@ -20,48 +20,48 @@ def main():
       tn = telnetlib.Telnet(HOST, PORT)
 
       time.sleep(1)
-      # print(tn.read_very_eager().decode('latin-1'))
+      tn.read_very_eager().decode('latin-1')
 
       # Enviar usuario
       tn.read_until("Operador".encode('latin-1'))
       tn.write(b"40884366\r\n")
       time.sleep(2)
-      # print(tn.read_very_eager().decode('latin-1'))
+      print(tn.read_very_eager().decode('latin-1'))
       
       # Enviar password
       tn.write(b"123456\r\n")
       time.sleep(5)
-      # print(tn.read_very_eager().decode('latin-1'))
+      print(tn.read_very_eager().decode('latin-1'))
 
       # Enter letters menu
       time.sleep(5)
       # tn.read_until("PRINCIPAL".encode('latin-1'))
       tn.write(b"\x10")
       tn.write(b"\x10")
-      # print(tn.read_very_eager().decode('latin-1'))
+      print(tn.read_very_eager().decode('latin-1'))
 
       # Enter letter c
       time.sleep(5)
       # tn.read_until("TECLAS".encode('latin-1'))
       tn.write(b"c\r\n")
-      # print(tn.read_very_eager().decode('latin-1'))
+      print(tn.read_very_eager().decode('latin-1'))
 
       # Enter 1 (Funciones de terminal)
       time.sleep(5)
       # tn.read_until("Terminal".encode('latin-1'))
-      tn.write(b"1\r\n")
-      # print(tn.read_very_eager().decode('latin-1'))
+      tn.write(b"7\r\n")
+      print(tn.read_very_eager().decode('latin-1'))
 
       # Enter 7 (Cargar Memoria del Terminal)
       time.sleep(5)
       tn.write(b"7\r\n")
-      # print(tn.read_very_eager().decode('latin-1'))
+      tn.read_very_eager().decode('latin-1')
 
       # Enter ID caja
       time.sleep(5)
       tn.write(b"103\r\n")
       time.sleep(2)
-      # print(tn.read_very_eager().decode('latin-1'))
+      tn.read_very_eager().decode('latin-1')
 
       tn.close()
 
