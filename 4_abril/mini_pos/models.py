a7d4fc53cc98f1d938e8b0ddd97b78b1a8b6b03b
Jose Castellon <josecastellon@MacBook-Air-de-Jose.local>
2026-04-06 22:13:19 -0500

    Se termina el proyecto del inventario y se continua con el proyecto nuevo de mini pos

---
+++ 4_abril/mini_pos/models.py
@@ -0,0 +1,20 @@
+class Product:
+	def __init__(self, id, name, price, stock, status):
+		if not name:
+			raise ValueError("Name cannot be empty")
+
+		if stock < 0:
+			raise ValueError("Stock cannot be negative")
+
+		if price <= 0:
+			raise ValueError("Price must be greater than 0")
+		
+		self.id = id
+		self.name = name
+		self.price = price
+		self.stock = stock
+		self.status = status
+
+class Sale:
+	def __init__(self):
+		pass
